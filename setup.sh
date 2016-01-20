86941cfd15695627aaac81f03cab576871f011aa
Matt Glaman <nmd.matt@gmail.com>
2016-01-20 00:44:08 -0600

    Have nginx mount phabricator container volumes
    Solves issues on mounting, and configuration being goofy.

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-git clone https://github.com/phacility/libphutil.git \
-  && git clone https://github.com/phacility/arcanist.git \
-  && git clone https://github.com/phacility/phabricator.git
-docker-compose build
-docker-compose up
